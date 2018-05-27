456f392d34547b2c028688fced9de7e725fe007c
ssyijiu <lxmyijiu@163.com>
2018-05-28 02:25:00 +0800

    Add item ripple

--- common/src/main/java/io/xdag/common/base/ToolbarActivity.java
+++ common/src/main/java/io/xdag/common/base/ToolbarActivity.java
@@ -52,6 +52,7 @@ public abstract class ToolbarActivity extends BaseActivity {
                         onBackPressed();
                         break;
                     default:
+
                 }
             }
         });
