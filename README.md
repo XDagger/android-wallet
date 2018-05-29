437ad59069b06f6499c77f5835142456e3622aee
lixiaoming <lixiaoming01@huaxiafinance.com>
2018-05-29 17:58:09 +0800

    Update home UI

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # xdagwallet
 xdag android wallet
-
-滑动嵌套：https://www.race604.com/android-nested-scrolling/
-
-http://blog.cgsdream.org/2016/12/30/android-nesting-scroll/
--- app/src/main/java/io/xdag/xdagwallet/fragment/HomeFragment.java
+++ app/src/main/java/io/xdag/xdagwallet/fragment/HomeFragment.java
@@ -1,9 +1,12 @@
 package io.xdag.xdagwallet.fragment;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import butterknife.BindView;
+import io.xdag.common.Common;
+import io.xdag.common.base.BaseFragment;
 import io.xdag.common.base.RefreshFragment;
 import io.xdag.common.base.ToolbarActivity;
 import io.xdag.common.util.ToastUtil;
@@ -15,26 +18,35 @@ import io.xdag.xdagwallet.adapter.TransactionAdapter;
  * <p>
  * desc :
  */
-public class HomeFragment extends RefreshFragment {
+public class HomeFragment extends BaseFragment {
 
     @BindView(R.id.home_rv)
     RecyclerView mRecyclerView;
+    @BindView(R.id.collapsing_toolbar)
+    CollapsingToolbarLayout mCollapsingToolbarLayout;
+
+    private double mBalance = 10792.7;
+
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
     }
+
 
     @Override
     protected int getLayoutResId() {
         return R.layout.fragment_home;
     }
 
+
     @Override
     protected void initView(View rootView) {
         super.initView(rootView);
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setAdapter(new TransactionAdapter());
+        mCollapsingToolbarLayout.setTitle(String.format("%s XDAG", mBalance));
     }
+
 
     @Override
     public void onHiddenChanged(boolean hidden) {
@@ -42,11 +54,5 @@ public class HomeFragment extends RefreshFragment {
         if (!hidden) {
             ((ToolbarActivity) mContext).mToolbar.setVisibility(View.GONE);
         }
-    }
-
-    @Override
-    public void onRefresh() {
-        ToastUtil.show("refresh");
-        completeRefresh();
     }
 }
