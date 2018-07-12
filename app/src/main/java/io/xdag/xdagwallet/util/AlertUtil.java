c9232df338def407ef5ff90170dfea5452d2409a
ssyijiu <lxmyijiu@163.com>
2018-07-13 01:40:23 +0800

    Update alert UI

--- app/src/main/java/io/xdag/xdagwallet/util/AlertUtil.java
+++ app/src/main/java/io/xdag/xdagwallet/util/AlertUtil.java
@@ -1,17 +1,19 @@
 package io.xdag.xdagwallet.util;
 
 import android.app.Activity;
+
 import com.tapadoo.alerter.Alerter;
+
 import io.xdag.xdagwallet.R;
 
 /**
  * created by lxm on 2018/5/30.
- *
+ * <p>
  * desc :
  */
 public class AlertUtil {
 
-    private static final int ALERT_DURATION = 1500;
+    private static final int ALERT_DURATION = 2000;
 
 
     public static void show(Activity activity, int res) {
@@ -21,9 +23,11 @@ public class AlertUtil {
 
     public static void show(Activity activity, String message) {
         Alerter.create(activity)
-            .setDuration(ALERT_DURATION)
-            .setBackgroundColorRes(R.color.colorAccent)
-            .setText(message)
-            .show();
+                .setDuration(ALERT_DURATION)
+                .hideIcon()
+                .setTextAppearance(R.style.AlertTextStyle)
+                .setBackgroundColorRes(R.color.colorAccent)
+                .setText(message)
+                .show();
     }
 }
