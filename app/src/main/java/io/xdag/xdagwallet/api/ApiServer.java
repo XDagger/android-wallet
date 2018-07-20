49966ebdda99dc146624a083fc998324116b625d
lixiaoming <lixiaoming01@huaxiafinance.com>
2018-07-20 15:34:31 +0800

    Update xdag scan ip

--- app/src/main/java/io/xdag/xdagwallet/api/ApiServer.java
+++ app/src/main/java/io/xdag/xdagwallet/api/ApiServer.java
@@ -10,7 +10,7 @@ import io.xdag.xdagwallet.api.xdagscan.XdagScanApi;
  */
 public class ApiServer {
 
-    private static final String BASE_URL_XDAGSCAN = "http://13.251.91.123:8888/";
+    private static final String BASE_URL_XDAGSCAN = "http://139.99.124.100:8888/";
 
 
     public static XdagScanApi getApi() {
