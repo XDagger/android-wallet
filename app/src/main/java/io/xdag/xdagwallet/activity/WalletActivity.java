27a36fcd529cbb4181d30470a8524847cd8ad383
ssyijiu <lxmyijiu@163.com>
2018-07-22 02:26:25 +0800

    Add use explain

--- app/src/main/java/io/xdag/xdagwallet/activity/WalletActivity.java
+++ app/src/main/java/io/xdag/xdagwallet/activity/WalletActivity.java
@@ -3,14 +3,21 @@ package io.xdag.xdagwallet.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.OnClick;
+import io.xdag.common.Common;
 import io.xdag.common.base.BaseActivity;
+import io.xdag.common.util.TextStyleUtil;
 import io.xdag.xdagwallet.MainActivity;
 import io.xdag.xdagwallet.R;
 import io.xdag.xdagwallet.config.Config;
 
 public class WalletActivity extends BaseActivity {
+
+    @BindView(R.id.wallet_tv_explain_text)
+    TextView mTvExplain;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,7 +33,17 @@ public class WalletActivity extends BaseActivity {
 
     @Override
     protected void initView(View rootView, Bundle savedInstanceState) {
-
+        mTvExplain.setText(
+                new TextStyleUtil()
+                        .append("XDAG Android 钱包目前还处于内测版本\n")
+                        .append("使用请注意：\n")
+                        .append("请务必在其他位置备份好您的钱包文件，\n")
+                        .setForegroundColor(Common.getColor(R.color.RED))
+                        .setFontSize(18, true)
+                        .append("以防止因本钱包的 bug 造成您的财产损失。\n")
+                        .append("任何卸载、删除数据的行为，都将造成钱包文件丢失，且无法找回。\n")
+                        .create()
+        );
     }
 
 
