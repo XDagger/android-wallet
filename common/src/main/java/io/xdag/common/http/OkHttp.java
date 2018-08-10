38cf6a1d42bd7371d5c12f986408f3d525d6673d
ssyijiu <lxmyijiu@163.com>
2018-08-10 17:10:53 +0800

    update okhttp config

--- common/src/main/java/io/xdag/common/http/OkHttp.java
+++ common/src/main/java/io/xdag/common/http/OkHttp.java
@@ -17,19 +17,21 @@ public class OkHttp {
 
     private final OkHttpClient.Builder builder;
 
+
     OkHttp() {
 
         ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-                .tlsVersions(TlsVersion.TLS_1_2)
-                .cipherSuites(
-                        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-                        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-                        CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256)
-                .build();
+            .tlsVersions(TlsVersion.TLS_1_2)
+            .cipherSuites(
+                CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+                CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256)
+            .build();
 
         builder = new OkHttpClient.Builder()
-                .retryOnConnectionFailure(true)
-                .connectionSpecs(Collections.singletonList(spec));
+            .retryOnConnectionFailure(true)
+            // .connectionSpecs(Collections.singletonList(spec))
+        ;
 
         if (Common.isDebug()) {
             // chuck
@@ -37,6 +39,7 @@ public class OkHttp {
         }
 
     }
+
 
     public OkHttpClient build() {
         return builder.build();
