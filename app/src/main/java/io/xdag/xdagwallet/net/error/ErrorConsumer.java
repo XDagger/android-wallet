5410c290f889cabf698d67fdbe613a768b0a119c
ssyijiu <lxmyijiu@163.com>
2019-01-08 19:07:19 +0800

    Add error paese for transactions request

--- app/src/main/java/io/xdag/xdagwallet/net/error/ErrorConsumer.java
+++ app/src/main/java/io/xdag/xdagwallet/net/error/ErrorConsumer.java
@@ -1,9 +1,14 @@
 package io.xdag.xdagwallet.net.error;
 
 import android.app.Activity;
+import android.text.TextUtils;
+import com.google.gson.Gson;
 import io.reactivex.functions.Consumer;
 import io.xdag.common.tool.MLog;
 import io.xdag.xdagwallet.util.AlertUtil;
+import java.io.IOException;
+import okhttp3.ResponseBody;
+import retrofit2.HttpException;
 
 /**
  * created by lxm on 2018/7/19.
@@ -13,9 +18,12 @@ import io.xdag.xdagwallet.util.AlertUtil;
 public class ErrorConsumer implements Consumer<Throwable> {
 
     private Activity activity;
+    private static final Gson gson = new Gson();
+
 
     public ErrorConsumer() {
     }
+
 
     public ErrorConsumer(Activity activity) {
         this.activity = activity;
@@ -24,8 +32,24 @@ public class ErrorConsumer implements Consumer<Throwable> {
 
     @Override public void accept(Throwable throwable) {
         MLog.i(throwable.getMessage());
-        if(activity != null) {
-            AlertUtil.show(activity, throwable.getMessage());
+        String errorMessage = throwable.getMessage();
+        if (throwable instanceof HttpException) {
+            // parse error message
+            HttpException httpException = (HttpException) throwable;
+            try {
+                ResponseBody errorBody = httpException.response().errorBody();
+                if (errorBody != null) {
+                    ErrorResponse errorResponse = gson.fromJson(errorBody.string(),
+                        ErrorResponse.class);
+                    if (errorResponse != null && !TextUtils.isEmpty(errorResponse.message)) {
+                        errorMessage = errorResponse.message;
+                    }
+
+                }
+            } catch (IOException ignored) {
+            }
+
         }
+        AlertUtil.show(activity, errorMessage);
     }
 }
