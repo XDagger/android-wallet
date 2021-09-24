7bbfebae7944686201b0856c005b76f22b8a2cd7
qixuan <1124035186@qq.com>
2021-09-24 16:40:59 +0800

    delete invalid code

--- app/src/main/java/io/xdag/xdagwallet/activity/RestoreActivity.java
+++ app/src/main/java/io/xdag/xdagwallet/activity/RestoreActivity.java
@@ -1,7 +1,6 @@
 package io.xdag.xdagwallet.activity;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
